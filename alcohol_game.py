bdf98ff242e7a98cc705ef50b0068b121c4d7b95
ljo1010 <ljo111004@gmail.com>
2025-07-05 20:42:50 +0900

    이름 받기

--- alcohol_game.py
+++ alcohol_game.py
@@ -8,6 +8,7 @@ class alcohol_game:
         self.alcohol_limit = 0
         self.game_list = []
     
+    # 인트로 함수 너무 빨리 출력된다고 생각들면 추후 sleep 시간 조정가능
     def intro(self):
         intro = r"""
             .　∧∧　■
@@ -22,9 +23,25 @@ class alcohol_game:
         for line in intro.splitlines():
             print(line)
             time.sleep(0.3)
-            
+    
+    # 게임이 돌아가는 로직 구현
+    def play():
+        print("게임 시작")
+    
+    # 게임을 시작하는 함수
     def start(self):
+        # 순서 1. 인트로 2. 시작 여부 3. 이름 받기 4. 게임 종료
         self.intro()
         
-game = alcohol_game()
-game.start()
+        yes_or_no = input("게임을 시작할까요?(y/n): ")
+        if yes_or_no == 'y' or yes_or_no == 'Y':
+            self.name = input("오늘 거하게 취해볼 당신의 이름은? : ").strip()
+            self.game_list.append(self.name)
+            
+            self.play()
+        else:
+            print("게임이 시작되지 못했습니다 ㅠ")
+            
+if __name__ == "__main__":
+    game = alcohol_game()
+    game.start()
